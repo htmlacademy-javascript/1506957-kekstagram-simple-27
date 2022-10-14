31f4c00348e45e516212896dd7db47336f5dc39a
busarka <busarka@gmail.com>
2022-10-15 01:44:10 +0500

    Сделала рефакторинг генерации 25 объектов

--- js/main.js
+++ js/main.js
@@ -7,30 +7,16 @@ const getRandomNumber = (min, max) => {
     const temp = max;
     max = min;
     min = temp;
-    return Math.floor(Math.random() * (max - min) + min);
   }
-  else {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+  return Math.floor(Math.random() * (max - min) + min);
 };
 getRandomNumber();
 
 //Функция для проверки максимальной длины строки
-// const maxStringLength = (string, maxLength) => string.length < maxLength;
-// maxStringLength();
-
+//const maxStringLength = (string, maxLength) => string.length < maxLength;
+//maxStringLength();
 
 const ELEMENTS_COUNTS = 25;
-
-// const ID_COUNTS = {
-//   min: 1,
-//   max: 25
-// }
-
-const URL_ADDRESS = {
-  min: 1,
-  max: 25
-};
 
 const DESCRIPTION_FOR_PHOTO = [
   'Это я на берегу моря',
@@ -43,8 +29,7 @@ const DESCRIPTION_FOR_PHOTO = [
 const LIKES_COUNT = {
   min: 15,
   max: 200
-// eslint-disable-next-line semi
-}
+};
 
 const COMMENTS_COUNT = {
   min: 0,
@@ -55,13 +40,12 @@ const getRandomElement = (elements) => (elements[getRandomNumber(0, elements.len
 
 const createElement = (index) => ({
   id: index,
-  url: `photos/${getRandomNumber(URL_ADDRESS.min, URL_ADDRESS.max)}.jpg`,
+  url: `photos/${index}.jpg`,
   description: getRandomElement(DESCRIPTION_FOR_PHOTO),
   likes: getRandomNumber(LIKES_COUNT.min, LIKES_COUNT.max),
   comments: getRandomNumber(COMMENTS_COUNT.min, COMMENTS_COUNT.max)
 });
 
-// eslint-disable-next-line no-shadow
 const similarElements = (index) => [...Array(index)].map((_item, index) => createElement(index + 1));
-// eslint-disable-next-line no-console
-console.log(similarElements(ELEMENTS_COUNTS));
+const similarElementsArray = similarElements(ELEMENTS_COUNTS);
+console.log(similarElementsArray);
