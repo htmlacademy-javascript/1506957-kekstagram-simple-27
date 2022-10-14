828016bcde42aa502765903f01c44a4a7128238c
busarka <busarka@gmail.com>
2022-10-15 00:29:25 +0500

    Сделала генерацию 25 объектов

--- js/main.js
+++ js/main.js
@@ -16,6 +16,52 @@ const getRandomNumber = (min, max) => {
 getRandomNumber();
 
 //Функция для проверки максимальной длины строки
-const maxStringLength = (string, maxLength) => string.length < maxLength;
+// const maxStringLength = (string, maxLength) => string.length < maxLength;
+// maxStringLength();
 
-maxStringLength();
+
+const ELEMENTS_COUNTS = 25;
+
+// const ID_COUNTS = {
+//   min: 1,
+//   max: 25
+// }
+
+const URL_ADDRESS = {
+  min: 1,
+  max: 25
+};
+
+const DESCRIPTION_FOR_PHOTO = [
+  'Это я на берегу моря',
+  'Мой кот Барсик, друг Кекса',
+  'Мы с ребятами на шашлыках в парке',
+  'Мариночка, с Днем Рождения!',
+  'Чудесный закат...'
+];
+
+const LIKES_COUNT = {
+  min: 15,
+  max: 200
+// eslint-disable-next-line semi
+}
+
+const COMMENTS_COUNT = {
+  min: 0,
+  max: 200
+};
+
+const getRandomElement = (elements) => (elements[getRandomNumber(0, elements.length - 1 )]);
+
+const createElement = (index) => ({
+  id: index,
+  url: `photos/${getRandomNumber(URL_ADDRESS.min, URL_ADDRESS.max)}.jpg`,
+  description: getRandomElement(DESCRIPTION_FOR_PHOTO),
+  likes: getRandomNumber(LIKES_COUNT.min, LIKES_COUNT.max),
+  comments: getRandomNumber(COMMENTS_COUNT.min, COMMENTS_COUNT.max)
+});
+
+// eslint-disable-next-line no-shadow
+const similarElements = (index) => [...Array(index)].map((_item, index) => createElement(index + 1));
+// eslint-disable-next-line no-console
+console.log(similarElements(ELEMENTS_COUNTS));
