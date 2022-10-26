dca5fec7f9e5cf4ebe17167ddec22468c87c043e
busarka <busarka@gmail.com>
2022-10-27 01:33:19 +0500

    Сделала отрисовку фото, лайков, комментариев

---
+++ js/render.js
@@ -0,0 +1,16 @@
+import { similarElements } from './data.js';
+
+const similarListElement = document.querySelector('.pictures');
+const similarElementTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const similarPhoto = similarElements;
+const similarListFragment = document.createDocumentFragment();
+
+similarPhoto.forEach(({url, likes, comments}) => {
+  const photoElement = similarElementTemplate.cloneNode(true);
+  photoElement.querySelector('.picture__img').src = url;
+  photoElement.querySelector('.picture__likes').textContent = likes;
+  photoElement.querySelector('.picture__comments').textContent = comments;
+  similarListFragment.append(photoElement);
+});
+similarListElement.append(similarListFragment);
